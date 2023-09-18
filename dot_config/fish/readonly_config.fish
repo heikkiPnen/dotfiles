59bbd715b3668de16b5a02ff2d35a8ce82f1c6fc
Heikki Pitkänen <heikki.pitkanen@iki.fi>
2023-09-18 17:17:16 +0300

    fish ja .bashrc

--- dot_config/fish/readonly_config.fish
+++ dot_config/fish/readonly_config.fish
@@ -1,8 +1,13 @@
 if status is-interactive
 # Vi -sidokset on määritelty .inpurc:ssä
     fish_vi_key_bindings
-    zoxide init fish | source
+    #zoxide init fish | source
     starship init fish | source
+    # fzf.fish plugin:
+    set fzf_directory_opts --bind "ctrl-o:execute(nvim {} &> /dev/tty)"
+    set fzf_preview_dir_cmd eza --all --color=always
+    fzf_configure_bindings --history=\ch  # c-r on varattu mcfly:lle
     mcfly init fish | source
+
 end
 
