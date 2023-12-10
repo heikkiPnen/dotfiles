d2b0169465a77df7402b63ae1e71e8c46bf16581
Heikki Pitkänen <heikki.pitkanen@iki.fi>
2023-12-10 10:57:33 +0200

    nvim

--- dot_config/nvim/lua/plugins/luasnip_asetukset.lua
+++ dot_config/nvim/lua/plugins/luasnip_asetukset.lua
@@ -1,50 +1,20 @@
 local M = {
-  "L3MON4D3/LuaSnip",
-
-  dependencies = {
-    "rafamadriz/friendly-snippets",
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
-    end,
-  },
+   "L3MON4D3/LuaSnip",
+   -- follow latest release.
+   version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+   -- install jsregexp (optional!).
+   build = "make install_jsregexp"
 }
+M.config = function()
+   local ls = require("luasnip")
+   vim.keymap.set({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
+   vim.keymap.set({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true })
+   vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(-1) end, { silent = true })
 
---function M.config()
---  local util = require("util")
---  local luasnip = require("luasnip")
---  require("neogen")
---
---  luasnip.config.setup({
---    history = true,
---    enable_autosnippets = true,
---    -- Update more often, :h events for more info.
---    -- updateevents = "TextChanged,TextChangedI",
---  })
---
---  --- <tab> to jump to next snippet's placeholder
---  local function on_tab()
---    return luasnip.jump(1) and "" or util.t("<Tab>")
---  end
---
---  --- <s-tab> to jump to next snippet's placeholder
---  local function on_s_tab()
---    return luasnip.jump(-1) and "" or util.t("<S-Tab>")
---  end
---
---  -- vim.keymap.set("i", "<Tab>", on_tab, { expr = true })
---  -- vim.keymap.set("s", "<Tab>", on_tab, { expr = true })
---  -- vim.keymap.set("i", "<S-Tab>", on_s_tab, { expr = true })
---  -- vim.keymap.set("s", "<S-Tab>", on_s_tab, { expr = true })
---
---  vim.cmd([[
---" press <Tab> to expand or jump in a snippet. These can also be mapped separately
---" via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
---imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
---" -1 for jumping backwards.
---inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
---snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
---snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
---]])
---end
-
+   vim.keymap.set({ "i", "s" }, "<C-E>", function()
+      if ls.choice_active() then
+         ls.change_choice(1)
+      end
+   end, { silent = true })
+end
 return M
