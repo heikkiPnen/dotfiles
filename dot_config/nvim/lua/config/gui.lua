efae02c548454378c65a389da936c26f9eed69eb
Heikki Pitkänen <heikki.pitkanen@iki.fi>
2024-01-06 17:51:04 +0200

    noice lisäys

--- dot_config/nvim/lua/config/gui.lua
+++ dot_config/nvim/lua/config/gui.lua
@@ -1,10 +1,12 @@
 if vim.g.neovide then
+  vim.g.neovide_theme = 'auto'
   vim.opt.guifont = { "JetBrains Mono", ":h13" }
-  vim.g.neovide_cursor_vfx_mode = "railgun"
   vim.g.NeovideMultiGrid = true
   vim.g.remember_window_size = true
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+  vim.g.neovide_cursor_vfx_particle_phase = 1.5
   vim.g.neovide_cursor_animate_command_line = true
   vim.g.neovide_cursor_vfx_particle_density = 7.0
-  vim.g.neovide_cursor_vfx_particle_lifetime = 2.0
+  vim.g.neovide_cursor_vfx_particle_lifetime = 1.0
   vim.g.neovide_cursor_vfx_particle_speed = 10.0
 end
