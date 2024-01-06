0327c8b0a878fc74e360b25c2fd028eea5d613ea
Heikki Pitkänen <heikki.pitkanen@iki.fi>
2024-01-06 16:28:32 +0200

    neovide muutos

--- dot_config/nvim/lua/config/gui.lua
+++ dot_config/nvim/lua/config/gui.lua
@@ -1,7 +1,5 @@
---vim.opt.guifont="JetBrains Mono Regular:h16"
-vim.opt.guifont = { "JetBrainsMono Nerd Font", ":h15" }
 if vim.g.neovide then
-  --vim.opt.guifont = { "JetBrains Mono", ":h13" }
+  vim.opt.guifont = { "JetBrains Mono", ":h13" }
   vim.g.neovide_cursor_vfx_mode = "railgun"
   vim.g.NeovideMultiGrid = true
   vim.g.remember_window_size = true
