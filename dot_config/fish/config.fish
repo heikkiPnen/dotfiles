0da29419450081019e5253c6bbb95bea806945ba
Heikki Pitkänen <heikki.pitkanen@iki.fi>
2024-01-22 14:30:35 +0200

    atuin setup

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,4 +1,4 @@
-#if status is-interactive
+if status is-interactive
 ## Vi -sidokset on määritelty .inpurc:ssä
 #    fish_vi_key_bindings
 #    #zoxide init fish | source
@@ -8,6 +8,6 @@
 #    set fzf_preview_dir_cmd eza --all --color=always
 #    fzf_configure_bindings --history=\ch  # c-r on varattu mcfly:lle
 #    #mcfly init fish | source
-#
-#end
+    atuin init fish | source
+end
 
