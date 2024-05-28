128a3daf3c949ed5af7ba085f79d88a16588b8f2
Heikki Pitkänen <heikki.pitkanen@iki.fi>
2024-05-28 17:30:25 +0300

    uusi yazi

---
+++ dot_config/fish/functions/lyhyt_kde_shortcuts.fish
@@ -0,0 +1,4 @@
+function lyhyt_kde_shortcuts
+    mdt /home/heikki/Tekstit/md/Zettelkasten/KDE_20211205.md
+end
+
--- dot_config/fish/functions/readonly_lyhyt_kde_shortcuts.fish
+++
@@ -1,4 +0,0 @@
-function lyhyt_kde_shortcuts
-    w3m /home/heikki/Dokumentit/html/Zettelkasten/KDE_20211205.html
-end
-
